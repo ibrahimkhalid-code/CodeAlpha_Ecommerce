6102c77147d1c633eba864af96c2d179d7254c23
ibrahimkhalid-code <68294799+ibrahimkhalid-code@users.noreply.github.com>
2025-12-26 23:36:16 +0200

    change

---
+++ js/main.js
@@ -0,0 +1,5 @@
+let categoryNavList = document.querySelector(".categoryNavList");
+let categoryBtn = document.querySelector(".catogryBtn");
+categoryBtn.addEventListener("click", () => {
+  categoryNavList.classList.toggle("visible");
+});
