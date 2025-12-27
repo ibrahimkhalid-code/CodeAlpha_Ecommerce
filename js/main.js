6e51878ff6e5176475870ed7353beb23a06732bd
ibrahimkhalid-code <68294799+ibrahimkhalid-code@users.noreply.github.com>
2025-12-27 13:29:49 +0200

    slider

--- js/main.js
+++ js/main.js
@@ -4,3 +4,17 @@ let categoryBtn = document.querySelector(".catogryBtn");
 categoryBtn.addEventListener("click", () => {
   categoryNavList.classList.toggle("visible");
 });
+
+// <!-- Initialize Swiper -->
+var swiper = new Swiper(".slideSwp", {
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+    clickable: true,
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  loop: true,
+});
